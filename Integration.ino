b4be08775e238fb3d55df1912cbdeac5b370419e
Cipher032 <solowingpixycipher@gmail.com>
2017-05-24 14:03:01 -0700

    Replaced with actual final code

--- Integration.ino
+++ Integration.ino
@@ -1,33 +1,28 @@
 #include <EYW.h>
-#include <Servo.h>
-#include <Wire.h>
-//define things here, everything else should run automatically
-//-------------------------- CAMERA SUBTEAM
-int pinNum = 1; //red wire
-int signalPin1 = 9; // white wire
-int angleHome = 0;
-int holdTime = 2000; //time in between parts
-int angle1 = 30;
-int angle2 = -30;
-int angle3 = 30;
-int delay1 = 2000; //time holding down
-int delay2 = 2000; //time holding down
-int delay3 = 2000; //time holding down
-Servo servo;
-//--------------------------- STRUCTURE SUBTEAM
-float drop_height = 0; //input initial drop height
-float current_height = 0; //input initial drop height
-EYW::Altimeter a;
-//--------------------------- DESCENT SUBTEAM
-int signalPin2=5;
-int deltaV=0;
-int idealDeltaV=3;
-int i=0;
-int heights[dropHeight/idealDeltaV];
-//--------------------------- END OF DEFINING
-void alarm(int i){
-while(heights[i]==heights[i-1]&&lifted==true&&dropped==true){
-    switch(height[i-2]-height[i-1]){
+  EYW::Altimeter a;
+void setup(){
+  Serial.begin(9600);
+  Serial.println("Initializing.....");
+  Serial.print(a.begin());
+  a.calibrate(10);
+  pinMode(5,OUTPUT);
+}
+boolean lifted=false,dropped=false;
+int height[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+void loop(){
+ for(int i=0;i<10;i++){
+ Serial.print("Measuring Altitude: ");
+ height[i]=(int)a.getHeightAvg(10);
+ Serial.println(height[i]);
+ if(height[i-1]<height[i]){
+  lifted=true;
+ }
+ if(height[i-1]>height[i]){
+  dropped=true;
+ }
+ int speed=(height[i-3]-height[i-2])*2;
+ while(height[i]==height[i-1]&&lifted==true&&dropped==true){
+    switch(speed%10){
       case 1:
       a.alarm(1,880,250);
       delay(10);
@@ -77,71 +72,15 @@ while(heights[i]==heights[i-1]&&lifted==true&&dropped==true){
       default:
       a.led(1);
     }
-    delay(1000);
+    
+    delay(1500);
+    if(a.getHeightAvg(10)>height[i]){
+      lifted=false;
+      int height[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+      i=0;
+    }
+ 
 }
- delay(1000);
+ delay(500);
 }
 }
-//---------------------------VOID SETUP
-void setup() {
-
-//--------------------------- CAMERA SUBGROUP CODE
-  servo.attach(signalPin1); // attach the signal pin of servo to signalPin1 of arduino, white wire
-  // part 1, hold on/off for 1.5s
-  for(int angle = angleHome; angle <= angle1; angle ++){    // command to move from angleHome degrees to angle1 degrees
-    servo.write(angle);                 //command to rotate the servo to the specified angle
-    delay(15);                       
-  } 
-  delay(delay1); //holds button for delay1 time
-  for(int angle = angle1; angle >= angleHome; angle--){     // command to move from angle1 degrees to angleHome degrees
-    servo.write(angle);              //command to rotate the servo to the specified angle
-    delay(15);                       
-  }
-    delay(holdTime); //stays at angleHome for holdTime time
-  // part 2, press mode button
-  for (int angle = angleHome; angle <= angle2; angle ++){    // command to move from angleHome degrees to angle2 degrees
-    servo.write(angle);                 //command to rotate the servo to the specified angle
-    delay(15);                       
-  } 
-  delay(delay2);         //holds button for delay2 time
-  for(int angle = angle2; angle >= angleHome; angle--){     // command to move from angle1 degrees to angleHome degrees
-    servo.write(angle);              //command to rotate the servo to the specified angle
-    delay(15);                       
-  } 
-    delay(holdTime); //stays at angleHome for holdTime time
-  // part 3, click on/off to take photo
-  for(int angle = angleHome; angle <= angle3; angle ++){    // command to move from angleHome degrees to angle2 degrees
-    servo.write(angle);                 //command to rotate the servo to the specified angle
-    delay(15);                       
-  }
-  delay(delay3); //holds button for delay2 time
-  for(int angle = angle3; angle >= angleHome; angle--){     // command to move from angle1 degrees to angleHome degrees
-    servo.write(angle);              //command to rotate the servo to the specified angle
-    delay(15);                    
-  }//stays at rest forever
-//--------------------------- STRUCTURE SUBGROUP CODE
-  Serial.begin(9600);
-  a.begin();
-  a.calibrate(100); //takes first 100 measurements as calibration
-  a.alarm();
-//--------------------------- DESCENT SUBGROUP CODE
-  pinMode(signalPin2,OUTPUT);
-  
-} //--------------------------- END OF VOID SETUP
-
-//---------------------------------------- VOID LOOP
-void loop(){
-  //--------------------------- CAMERA SUBGROUP CODE
-  //--------------------------- STRUCTURE SUBGROUP CODE
-  current_height = a.getHeightAvg (20);//takes the average of 20 measurements to get current height
-  Serial.print("Current Height: "+current_height+"");
-  if (current_height > drop_height) {
-  a.alarm(6,2000,500); } //intiates the drop mechanism
-  //--------------------------- DESCENT SUBGROUP CODE
-  if (altitude==0){
-   a.alarm(heights[i-1]-heights[i]);//gonna have to overwrite the alarm method here
-  }
-  heights[i]=a.getHeightAvg(20);
-  i++;
-  delay(1000);
-} //--------------------------- END OF VOID LOOP
